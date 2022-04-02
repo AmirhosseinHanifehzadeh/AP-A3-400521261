795ef1e688ae606c49dd20c8fd5136a9e3f2af3b
AmirHossein Hanifeh <hanifehzadeham@outlook.com>
2022-04-02 04:34:10 +0430

    class Library added

--- question2/question2.py
+++ question2/question2.py
@@ -37,3 +37,33 @@ class LibraryMember:
             for borrowedBook in member['borrowedBooks']:
                 print(f"\t- {borrowedBook['name']} {borrowedBook['id']}")
         print('----------------------------------------------------------')
+
+class Library:
+    @staticmethod
+    def get(member_id, book_id):
+        for member in LibraryMember.members:
+            if member['id'] == member_id:
+                if len(member['borrowedBooks']) > 4:
+                    raise ValueError(f"MaxReached : {member['name']} {member['id']}")
+
+                else:
+                    for book in Book.books:
+                        if book['id'] == book_id:
+                            if book['count'] == 0:
+                                raise ValueError(f"NotAvailable : {book['name']} {book['id']}")
+                            else:
+                                book['count'] -= 1
+                                member['borrowedBooks'].append(book_id)
+                                break
+                    break
+
+    @staticmethod
+    def Return(member_id, book_id):
+        for member in LibraryMember.members:
+            if member['id'] == member_id:
+                member['borrowedBooks'].remove(book_id)
+                break
+        for book in Book.books:
+            if book['id'] == book_id:
+                book['count'] += 1
+                break
